4dd74190af36401466ad046a3c8a2bf2bf928def
João Victor da Silva <jv782063@gmail.com>
2021-05-05 15:22:28 -0300

    some more initial settings

---
+++ application/views/home.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+  </head>
+  <body>
+    <h1>Hello</h1>
+  </body>
+</html>
